3940b32bc3cb9ed93fc76c532c4dd09651bd1fc3
vatrick <muhammadvatrick@students.amikom.ac.id>
2024-01-03 02:07:04 +0700

    first commit

---
+++ data-clean.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import re
+
+# Baca file CSV
+data = pd.read_csv('bukukita/data-buku.csv', encoding='utf-8')
+
+# Hapus karakter khusus dari kolom 'Penerbit'
+data['Penerbit'] = data['Penerbit'].apply(lambda x: re.sub(r"[^\w\s]", "", str(x)))
+
+# Hapus karakter khusus dari kolom 'Text Bahasa'
+data['Text Bahasa'] = data['Text Bahasa'].apply(lambda x: re.sub(r"[^\w\s]", "", str(x)))
+
+# Simpan ke file Excel
+data.to_excel('data-buku.xlsx', index=False)
